4e3d0ee94dd6ea98727c83c7187eee1f5787b4b4
aryasuresh123123 <165830928+aryasuresh123123@users.noreply.github.com>
2024-04-02 23:57:22 +0530

    Add files via upload

---
+++ index.jsx
@@ -0,0 +1,36 @@
+import "./index.css";
+import { Link } from "react-router-dom";
+
+export default function RandomPic() {
+  return (
+    <>
+      <div className="main-content">
+        <div className="random-navbar">
+          <div className="random-home-link">
+            <Link to="/">Home</Link>
+          </div>
+        </div>
+        <div className="random-pic-grid">
+          <div className="grid-item">
+            <img src="https://random.imagecdn.app/500/350" />
+          </div>
+          <div className="grid-item">
+            <img src="https://random.imagecdn.app/499/350" />
+          </div>
+          <div className="grid-item">
+            <img src="https://random.imagecdn.app/500/349" />
+          </div>
+          <div className="grid-item">
+            <img src="https://random.imagecdn.app/501/351" />
+          </div>
+          <div className="grid-item">
+            <img src="https://random.imagecdn.app/502/350" />
+          </div>
+          <div className="grid-item">
+            <img src="https://random.imagecdn.app/500/352" />
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
