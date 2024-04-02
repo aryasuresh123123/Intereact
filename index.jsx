315aed963ba665acf24d8700d6da4ceb9fa71391
aryasuresh123123 <165830928+aryasuresh123123@users.noreply.github.com>
2024-04-03 00:02:58 +0530

    Add files via upload

--- index.jsx
+++ index.jsx
@@ -1,36 +1,48 @@
-import "./index.css";
 import { Link } from "react-router-dom";
+import './index.css';
 
-export default function RandomPic() {
-  return (
-    <>
-      <div className="main-content">
-        <div className="random-navbar">
-          <div className="random-home-link">
-            <Link to="/">Home</Link>
-          </div>
-        </div>
-        <div className="random-pic-grid">
-          <div className="grid-item">
-            <img src="https://random.imagecdn.app/500/350" />
-          </div>
-          <div className="grid-item">
-            <img src="https://random.imagecdn.app/499/350" />
-          </div>
-          <div className="grid-item">
-            <img src="https://random.imagecdn.app/500/349" />
-          </div>
-          <div className="grid-item">
-            <img src="https://random.imagecdn.app/501/351" />
-          </div>
-          <div className="grid-item">
-            <img src="https://random.imagecdn.app/502/350" />
-          </div>
-          <div className="grid-item">
-            <img src="https://random.imagecdn.app/500/352" />
-          </div>
-        </div>
-      </div>
-    </>
-  );
+export default function HomePage(){
+    return(
+        <>
+            <div className="main-content">
+                <div className="video-container">
+                    <video autoPlay muted loop playsInline id="vid">
+                        <source src='/vid.mp4' type='video/mp4' />
+                    </video>
+                </div>
+                <div className="nav-bar">
+                    <div className="nav-links">
+                        <div className="links">
+                            <div className="randompic-link">
+                                <Link to="/random">Random</Link>
+                            </div>
+                            <div className="login-link">
+                                <Link to="/login">Login</Link>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="name">INTEREACT</div>
+                <div className="about-section-container">
+                    <div className="about-content">
+                        <p>
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                            Officia, nobis magnam itaque fuga asperiores tenetur quo accusantium iste, 
+                            excepturi doloribus a! Quis aspernatur laboriosam numquam ea, natus velit fuga corporis, 
+                            repudiandae non recusandae quod incidunt? Alias magnam consequatur illum qui minus sit consectetur expedita ducimus iste et. 
+                            Nemo, repellendus doloribus.
+                        </p>
+                    </div>
+                </div>
+                <div className="footer-section">
+                    <div className="contacts">
+                        <div className="contact-title">Contacts</div>
+                        <div>Phno : 987654321</div>
+                        <a>Instagram: abc_10</a>
+                        <a>gmail: abc@gmail.com</a>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
 }
